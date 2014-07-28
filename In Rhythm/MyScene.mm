85188fbd78b1a46028ae7fe1c7f67a0d924f2ca9
Mark Meyer <mark.meyer37@gmail.com>
2014-07-27 21:38:20 -0700

    Tap to play. Dynamically add emitters

--- In Rhythm/MyScene.mm
+++ In Rhythm/MyScene.mm
@@ -14,6 +14,7 @@
     NSMutableArray *emitterArray;
     int birthRate;
     int emitterInsertPos;
+    int maxEmitters;
 }
 
 -(id)initWithSize:(CGSize)size {
@@ -21,79 +22,100 @@
         self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
         
         AudioPlayer *aplayer = [AudioPlayer sharedInstance];
-
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-        myLabel.text = [aplayer.track objectForKey:@"title"];
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
         
-        [self addChild:myLabel];
+        [self createLabels:aplayer.track];
         
         self->birthRate = 100;
         self->emitterInsertPos = 0;
-        self->emitterArray = [[NSMutableArray alloc] initWithCapacity:5];
+        self->maxEmitters = 5;
+        self->emitterArray = [[NSMutableArray alloc] initWithCapacity:self->maxEmitters];
     }
     return self;
 }
 
-- (void)didMoveToView:(SKView *)view {
-    AudioPlayer *aplayer = [AudioPlayer sharedInstance];
-    [aplayer.player play];
-}
+/*
+ - (void)didMoveToView:(SKView *)view {
+ AudioPlayer *aplayer = [AudioPlayer sharedInstance];
+ [aplayer.player play];
+ }
+ */
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        //add effect at touch location
-        if ([self->emitterArray count] == 5) {
-            SKEmitterNode *toRemove = self->emitterArray[self->emitterInsertPos];
-            [toRemove removeFromParent];
-            [self->emitterArray removeObjectAtIndex:self->emitterInsertPos];
+    AudioPlayer *aplayer = [AudioPlayer sharedInstance];
+    if (![aplayer.player isPlaying]) {
+        [aplayer.player play];
+        
+        for (UITouch *touch in touches) {
+            CGPoint location = [touch locationInNode:self];
+            [self addEmitter:location];
         }
-        SKEmitterNode *newEmitter = [self newExplosion:location.x : location.y];
-        [self addChild: newEmitter];
-        [self->emitterArray insertObject:newEmitter atIndex:self->emitterInsertPos];
-        self->emitterInsertPos = (self->emitterInsertPos + 1) % 5;
-        //self->birthRate = 100 / [self->emitterArray count];
     }
+}
+
+- (void) addEmitter:(CGPoint)location {
+    if ([self->emitterArray count] == self->maxEmitters) {
+        SKEmitterNode *toRemove = self->emitterArray[self->emitterInsertPos];
+        [toRemove removeFromParent];
+        [self->emitterArray removeObjectAtIndex:self->emitterInsertPos];
+    }
+    SKEmitterNode *newEmitter = [self newExplosion:location.x : location.y];
+    [self addChild: newEmitter];
+    [self->emitterArray insertObject:newEmitter atIndex:self->emitterInsertPos];
+    self->emitterInsertPos = (self->emitterInsertPos + 1) % self->maxEmitters;
 }
 
 - (SKEmitterNode *) newExplosion: (float)posX : (float) posy
 {
     SKEmitterNode *emitter =  [NSKeyedUnarchiver unarchiveObjectWithFile:[[NSBundle mainBundle] pathForResource:@"SparkParticle" ofType:@"sks"]];
     emitter.position = CGPointMake(posX,posy);
-    //emitter.name = @"explosion";
-    //emitter.targetNode = self.scene;
-    //emitter.zPosition=2.0;
     return emitter;
 }
 
 -(void)update:(CFTimeInterval)currentTime {
     AudioPlayer *aplayer = [AudioPlayer sharedInstance];
-    [aplayer.player updateMeters];
     
-    float scale = 0.5;
-    
-    float power = 0.0f;
-    for (int i = 0; i < [aplayer.player numberOfChannels]; i++) {
-        power += [aplayer.player averagePowerForChannel:i];
+    if ([aplayer.player isPlaying]) {
+        [aplayer.player updateMeters];
+        
+        float scale = 0.5;
+        float power = 0.0f;
+        
+        for (int i = 0; i < [aplayer.player numberOfChannels]; i++) {
+            power += [aplayer.player averagePowerForChannel:i];
+        }
+        power /= [aplayer.player numberOfChannels];
+        
+        float level = meterTable.ValueAt(power);
+        scale = level * 2;
+        
+        NSLog(@"Power: %f, Level: %f, Scale: %f, Birth Rate: %d", power, level, scale, self->birthRate);
+        
+        for (int i = 0; i < [self->emitterArray count]; ++i){
+            SKEmitterNode *emitterNode = self->emitterArray[i];
+            emitterNode.particleScale = scale;
+            emitterNode.particleBirthRate = self->birthRate * scale;
+        }
+        
+        if (scale > 1.9) {
+            [self addEmitter:CGPointMake( (CGFloat)(arc4random() % (int)self.scene.size.width), (CGFloat)(arc4random() % (int)self.scene.size.height))];
+        }
     }
-    power /= [aplayer.player numberOfChannels];
-    
-    float level = meterTable.ValueAt(power);
-    scale = level * 2;
-    
-    
-    NSLog(@"Power: %f, Level: %f, Scale: %f, Birth Rate: %d", power, level, scale, self->birthRate);
+}
 
+- (void) createLabels:(NSDictionary *)track {
+    SKLabelNode *titleLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+    titleLabel.text = [track objectForKey:@"title"];
+    titleLabel.fontSize = 30;
+    titleLabel.position = CGPointMake(CGRectGetMidX(self.frame),
+                                      CGRectGetMidY(self.frame));
+    [self addChild:titleLabel];
     
-    for (int i = 0; i < [self->emitterArray count]; ++i){
-        SKEmitterNode *emitterNode = self->emitterArray[i];
-        emitterNode.particleScale = scale;
-        emitterNode.particleBirthRate = self->birthRate * scale;
-    }
-     
+    SKLabelNode *artistLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+    artistLabel.text = [[track objectForKey:@"user"] objectForKey:@"username"];
+    artistLabel.fontSize = 20;
+    artistLabel.position = CGPointMake(CGRectGetMidX(self.frame),
+                                       CGRectGetMidY(self.frame) + 30);
+    [self addChild:artistLabel];
 }
 
 @end
